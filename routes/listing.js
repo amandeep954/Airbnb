a52582c2f8e2d991cfb391bb75fcc83d96630502
Aman Deep <ad707863@gmail.com>
2025-11-02 13:01:50 +0530

    fix: corrected typo in file imports

--- routes/listing.js
+++ routes/listing.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing");
-const wrapAsync = require("../utils/wrapAcync");
+const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware");
 const listingController = require("../controllers/listings");
 const multer = require("multer");
